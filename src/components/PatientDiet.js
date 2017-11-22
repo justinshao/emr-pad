c2c30596b6dea37ceb7acf3c0cfa40f8752197b5
justinshao <justinshao1258@outlook.com>
2017-11-22 17:18:50 +0800

    病人信息布局补充

--- src/components/PatientDiet.js
+++ src/components/PatientDiet.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import { contentStyle, titleStyle, tableHeader, tableContent } from '../styles';
+import { Table, TableBody, TableHeader, TableHeaderColumn, TableRow, TableRowColumn } from 'material-ui/Table';
+import NoResult from './NoResult';
 
 class PatientHospital extends React.Component {
 
