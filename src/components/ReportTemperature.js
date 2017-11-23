3d03eed1a5cc286a197fb8513be3c202a0ef11da
justinshao <justinshao1258@outlook.com>
2017-11-23 16:42:47 +0800

    实现侧滑翻页

--- src/components/ReportTemperature.js
+++ src/components/ReportTemperature.js
@@ -11,19 +11,69 @@ const contentStyle={
 };
 const tempContentStyle={
     'height':`${viewHeight-120}px`,
-    'overflow':'scroll'
+    'overflow':'scroll',
+    'position':'relative'
 };
 const imgStyle={
     'width':'94%',
     'display':'block',
     'margin':'auto'
 };
+const imgNextStyle={
+    'display':'block',
+    'opacity':'0.6',
+    'position':'absolute',
+    'top':'40%',
+    'right':'20px'
+};
+const imgBeforeStyle={
+    'display':'block',
+    'opacity':'0.6',
+    'position':'absolute',
+    'top':'40%',
+    'left':'20px'
+};
 
 class ReportTemperature extends React.Component {
     constructor(props){
         super(props);
         this.state={
-            src:''
+            src:'../../images/before.png',
+            startX:'',
+            startY:''
+        }
+        this.handleWillChangeImg=this.handleWillChangeImg.bind(this);
+        this.handleChangeImg=this.handleChangeImg.bind(this);
+    }
+
+    handleWillChangeImg(e){
+        e.preventDefault();
+        this.setState={
+            startX :e.pageX,
+            startY :e.pageY
+        }
+    }
+
+    handleChangeImg(e){
+        e.preventDefault();
+        let moveEndX = e.pageX;
+        let moveEndY = e.pageY;
+        let X = moveEndX - this.state.startX;
+        let Y = moveEndY - this.state.startY;
+        if ( Math.abs(X) > Math.abs(Y) && X > 0 ) {
+            alert('left to right');
+            this.setState={
+                // 改变src
+            }
+        }
+        else if ( Math.abs(X) > Math.abs(Y) && X < 0 ) {
+            alert('right to left');
+            this.setState={
+                // 改变src
+            }
+        }
+        else{
+            alert('else');
         }
     }
 
@@ -32,8 +82,10 @@ class ReportTemperature extends React.Component {
             true ? (
                 <div>
                     <div style={titleStyle}>体温单</div>
-                    <div style={tempContentStyle}>
-                        <img src='../../images/picCare.jpg' style={imgStyle}/>
+                    <div style={tempContentStyle} onTouchMove={this.handleChangeImg} onTouchStart={this.handleWillChangeImg}>
+                        <img src='../../images/picTemp.jpg' style={imgStyle}/>
+                        <img src={this.state.src} style={imgBeforeStyle}/>
+                        <img src='../../images/next.png' style={imgNextStyle}/>
                     </div>
                 </div>
             ) : <NoResult />
